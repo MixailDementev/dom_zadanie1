53e59b18764702778a280a4c79458c9402aca6d1
MDementev <vanneste1207@gmail.com>
2022-10-09 09:45:33 +0300

    5 задача

---
+++ zadanie5.py
@@ -0,0 +1,24 @@
+# Задача 5: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние
+# между ними в 2D пространстве.
+# Пример:
+# - A (3,6); B (2,1) -> 5,09
+# - A (7,-5); B (1,-1) -> 7,21
+import math
+
+x1 = float(input('enter the x coordinate of point A = '))
+y1 = float(input('enter the y coordinate of point A = '))
+x2 = float(input('enter the x coordinate of point B = '))
+y2 = float(input('enter the y coordinate of point B = '))
+
+# result = round(((x1-x2)**2+(y1-y2)**2)**0.5, 2)
+# result = round(math.sqrt(math.pow((x1-x2), 2)+math.pow((y1-y2), 2)), 3)
+# print(result)
+
+
+def distance(x1, y1, x2, y2):
+    result = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    return result
+
+
+result = round(distance(x1, y1, x2, y2), 3)
+print(result)
